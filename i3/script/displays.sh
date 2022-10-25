e0e497dd9e7621296d861d6d97d95bc09ba4b6ef
h-akira <79441679+h-akira@users.noreply.github.com>
2022-10-26 02:07:05 +0900

    update for release

--- i3/script/displays.sh
+++ i3/script/displays.sh
@@ -1,6 +1,4 @@
 # モニターの配置を行う
-
-# echo "$(xrandr | grep 'HDMI-1 connected')"
 if [ -n "$(xrandr | grep 'HDMI-1 connected')" ] && [ -n "$(xrandr | grep 'HDMI-2 connected')" ]; then
   xrandr --output HDMI-1 --auto --primary --output HDMI-2 --auto --left-of HDMI-1
 fi
